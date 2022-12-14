bd7134b5e01501b807038461a4fc3001d7e61ec5
Hugo Manzoni <hugocmdesign@gmail.com>
2022-12-14 11:50:23 +0000

    Add user list

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -5,20 +5,22 @@ import MessagesList from '../MessagesList';
 import { IMessagesChat } from '../../redux/types';
 import { ChatRoomType, FoundLastMsg } from './types';
 import useMessagesChat from '../../hooks/useMessagesChat';
+import UsersList from '../UserList';
 
 const ChatRoom: ChatRoomType = ({ currentUser }) => {
-  const { msgsChat } = useMessagesChat();
+  const { msgsChat } = useMessagesChat(currentUser);
   const foundLastMsg: FoundLastMsg = (user) =>
     msgsChat.find((msg: IMessagesChat) => msg.user === user);
 
   return (
-    <div className="container">
+    <div className="containerWithUsersList">
       <MessagesList currentUser={currentUser} msgsChat={msgsChat} />
       <TextInput
         isChat={true}
         currentUser={currentUser}
         foundLastMsg={foundLastMsg}
       />
+      <UsersList />
     </div>
   );
 };
