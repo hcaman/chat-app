e3f5edd46bfb91674dc8f8c9d0cf7ff4bd60034e
Hugo Manzoni <hugocmdesign@gmail.com>
2022-12-20 17:24:44 +0100

    update render method

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -8,17 +8,16 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 
-const root = document.getElementById('root') as HTMLElement;
-
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
 const persistor: Persistor = persistStore(store);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
